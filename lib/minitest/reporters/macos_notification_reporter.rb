7baa847d3eb7e4fbcbce7b8a923c113f19c35288
Stuart <stuart.chinery@gmail.com>
2018-04-04 09:59:50 +0100

    Added os gem so the notification will only be triggered on macs

--- lib/minitest/reporters/macos_notification_reporter.rb
+++ lib/minitest/reporters/macos_notification_reporter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/reporters"
+require "os"
 require "terminal-notifier"
 
 module Minitest
@@ -8,6 +9,12 @@ module Minitest
     class MacosNotificationReporter < BaseReporter
       def report
         super
+        show_notification if OS.mac?
+      end
+
+      private
+
+      def show_notification
         text = "#{total_count} tests, #{failures} failures, #{errors} errors, #{skips} skips in #{total_time}"
         title = options[:title] || "Minitest"
         subtitle = (passed? ? "\u{1F49A} Success" : "\u{1F494} Failed")
--- minitest-macos-notification.gemspec
+++ minitest-macos-notification.gemspec
@@ -25,6 +25,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "minitest", "~> 5.0"
   spec.add_dependency "minitest-reporters", "~> 1.1"
+  spec.add_dependency "os", "~> 1.0"
   spec.add_dependency "terminal-notifier", "~> 2.0"
 
   spec.add_development_dependency "bundler", "~> 1.16"
